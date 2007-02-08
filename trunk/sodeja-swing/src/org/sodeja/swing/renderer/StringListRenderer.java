b988231b5ac6ffb20e6db2f577b2e39cc907d223
sungon <sungon@ca6312be-f328-0410-97a9-af0f389fc61a>
2007-02-08 11:21:18 +0000

    cleanups

--- trunk/sodeja-swing/src/org/sodeja/swing/renderer/StringListRenderer.java
+++ trunk/sodeja-swing/src/org/sodeja/swing/renderer/StringListRenderer.java
@@ -6,6 +6,7 @@ import javax.swing.DefaultListCellRenderer;
 import javax.swing.JList;
 
 public abstract class StringListRenderer<T> extends DefaultListCellRenderer {
+	@SuppressWarnings("unchecked")
 	public Component getListCellRendererComponent(JList list, Object value,
 			int index, boolean isSelected, boolean cellHasFocus) {
 		return super.getListCellRendererComponent(list, getText((T) value), index, isSelected, cellHasFocus);
