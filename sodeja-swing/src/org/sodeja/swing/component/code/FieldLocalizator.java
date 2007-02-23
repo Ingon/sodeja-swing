09d6847c14fd72691316a14a304a55ae1928b40e
sungon <sungon@ca6312be-f328-0410-97a9-af0f389fc61a>
2007-02-23 11:11:14 +0000

    text localizators

---
+++ sodeja-swing/src/org/sodeja/swing/component/code/FieldLocalizator.java
@@ -0,0 +1,33 @@
+package org.sodeja.swing.component.code;
+
+import java.awt.GridBagConstraints;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+
+import org.sodeja.swing.GridBag;
+import org.sodeja.swing.component.action.CallLocalMethodAction;
+import org.sodeja.swing.component.form.FormPanelGridData;
+import org.sodeja.swing.context.ApplicationContext;
+import org.sodeja.swing.resource.ResourceConstants;
+
+public class FieldLocalizator<T extends ApplicationContext> extends TextLocalizator<T> {
+	
+	public FieldLocalizator(T ctx, JComponent container, FormPanelGridData gridData, String lblResource) {
+		super(ctx, container, gridData, lblResource);
+		initComponents();
+	}
+
+	protected void initComponents() {
+		container.add(ctx.getLocalizationFactory().createLabel(lblResource), 
+				GridBag.lineLabel(gridData.getRow()));
+		
+		tcValue = new JTextField();
+		container.add(tcValue, GridBag.lineField(gridData.getRow(), gridData.getColumnsCount() - 2));
+		
+		actionValue = new CallLocalMethodAction<T>(ctx, ResourceConstants.BTN_DICTIONARY, this, "dictionaryCallback"); //$NON-NLS-1$
+		container.add(new JButton(actionValue), GridBag.create(gridData.getColumnsCount() - 1, gridData.getRow(), 1, 1, 0.0, 0.0,
+				GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL));
+	}
+}
